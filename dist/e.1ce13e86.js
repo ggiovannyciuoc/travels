d2fbb7049534603705eeda96dafb4cf741bb53ab
gci_uoc <ggiovannyci@uoc.edu>
2021-10-29 17:16:22 +0200

    Primer commit

---
+++ dist/e.1ce13e86.js
@@ -0,0 +1,2 @@
+function menu(){var a=document.getElementById("barraNav");"nav"===a.className?a.className+=" responsive":a.className="nav"}
+//# sourceMappingURL=e.1ce13e86.js.map
---
+++ dist/e.c3d2da4f.js
@@ -0,0 +1,2 @@
+function menu(){var a=document.getElementById("barraNav");"nav"===a.className?a.className+=" responsive":a.className="nav"}
+//# sourceMappingURL=e.c3d2da4f.js.map
