fa39033adb82594aa11773861ea64bce0c6b0541
gci_uoc <ggiovannyci@uoc.edu>
2021-10-30 09:57:18 +0200

    .eslinttrc.json added

--- dist/e.1ce13e86.js
+++
@@ -1,2 +0,0 @@
-function menu(){var a=document.getElementById("barraNav");"nav"===a.className?a.className+=" responsive":a.className="nav"}
-//# sourceMappingURL=e.1ce13e86.js.map
--- dist/e.c3d2da4f.js
+++
@@ -1,2 +0,0 @@
-function menu(){var a=document.getElementById("barraNav");"nav"===a.className?a.className+=" responsive":a.className="nav"}
-//# sourceMappingURL=e.c3d2da4f.js.map
